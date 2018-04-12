855b748ae64bfa1877adb80f353e25287ba1e83e
Krosenblad <kajsa.rosenblad@student.uva.nl>
2018-04-12 13:40:16 +0200

    Voeg lisa toe

--- random.py
+++ random.py
@@ -1,8 +1,9 @@
 import random
 
-NAMES = ['Stephen', 'Daan', 'Kees', 'Dennis']
+NAMES = ['Stephen', 'Daan', 'Kees', 'Dennis', 'Lisa']
 
 def get_name():
 	return random.choice(NAMES)
+name = random.choice(NAMES)
 
 print("Hey ik ben {naam}".format(naam=get_name()))
